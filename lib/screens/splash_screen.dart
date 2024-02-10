dcedbad718debead6bfa689d0e3e3e933ec2147f
Jonathan de Gaston <developerjon5@gmail.com>
2024-02-10 09:34:29 -0700

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -88,6 +88,7 @@ class SplashScreenState extends State<SplashScreen> {
               });
             },
           ),
+          const SizedBox(height: BIGGER_PADDING_SIZE * 8),
         ],
       ),
     );
